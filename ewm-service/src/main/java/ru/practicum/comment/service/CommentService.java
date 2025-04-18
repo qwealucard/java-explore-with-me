f6d1523650b2f76e14be2d014f6f64e418b00cca
Артём <qwealucard@yandex.ru>
2025-04-18 19:32:30 +0300

    adjusted transactions

--- ewm-service/src/main/java/ru/practicum/comment/service/CommentService.java
+++ ewm-service/src/main/java/ru/practicum/comment/service/CommentService.java
@@ -6,23 +6,18 @@ import ru.practicum.comment.dto.CommentRequest;
 
 import java.util.List;
 
+@Transactional
 public interface CommentService {
 
-    @Transactional
     CommentDto createComment(CommentRequest commentRequest, Long userId, Long eventId);
 
-    @Transactional
     void deleteCommentPublic(Long commentId, Long userId);
 
-    @Transactional
     void deleteCommentAdmin(Long commentId);
 
-    @Transactional(readOnly = true)
     CommentDto getCommentById(Long commentId);
 
-    @Transactional(readOnly = true)
     List<CommentDto> getEventComments(Long eventId);
 
-    @Transactional(readOnly = true)
     List<CommentDto> getUserComments(Long userId);
 }
